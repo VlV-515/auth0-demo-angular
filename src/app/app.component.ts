21c328da72e2e8030ef810c580316c1869aea02b
VlV <92279938+VlV-515@users.noreply.github.com>
2023-05-25 19:21:53 -0600

    Feat->Add get token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,11 @@ import { AuthService } from '@auth0/auth0-angular';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  token: any = '';
   constructor(public readonly auth0Svc: AuthService) {}
   ngOnInit(): void {
     this.auth0Svc.isAuthenticated$.subscribe((isAuthenticated) => {
-      if (isAuthenticated) {
-        console.log('User is authenticated');
-      } else {
-        console.log('User is not authenticated');
-      }
+      if (isAuthenticated) this.getToken();
     });
   }
   btnLogin(): void {
@@ -22,5 +19,10 @@ export class AppComponent implements OnInit {
   }
   btnLogout(): void {
     this.auth0Svc.logout();
+  }
+  getToken(): void {
+    this.auth0Svc.idTokenClaims$.subscribe((token) => {
+      this.token = token?.__raw;
+    });
   }
 }
